d2abd5634939d6af1f03eb2c989f8b8a5c9098cf
Steven Vandegrift <svandegrift@icloud.com>
2024-03-21 17:48:52 -0700

    Integrate SQLAlchemy
    Added Flask-SQLAlchemy to the project

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from typing import TYPE_CHECKING
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'You wish you knew'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-task_list = ['Learn Flask', 'Learn SQL', 'Learn Django']
+db = SQLAlchemy(app)
+if TYPE_CHECKING:
+    from flask_sqlalchemy.model import Model
+    BaseModel = db.make_declarative_base(Model)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), index=True)
 
 class TaskForm(FlaskForm):
     task = StringField("Task", validators=[DataRequired()])
@@ -16,6 +28,15 @@ class TaskForm(FlaskForm):
 def index():
     task_form = TaskForm()
     if task_form.validate_on_submit():
-        task_list.append(task_form.task.data)
+        new_task = Task(task=task_form.task.data)
+        db.session.add(new_task)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('index.html', todos = task_list, template_form = task_form)
+    return render_template('index.html', todos = Task.query.all(), template_form = task_form)
+
+@app.route('/remove/<int:task_id>')
+def remove_task(task_id):
+    task_to_remove = Task.query.get(task_id)
+    db.session.delete(task_to_remove)
+    db.session.commit()
+    return redirect(url_for('index'))
---
+++ create_tasks.py
@@ -0,0 +1,16 @@
+from app import app, db, Task
+
+
+# first_task = Task(task="Learn Flask")
+# second_task = Task(task="Learn SQL")
+# third_task = Task(task="Learn Django")
+
+# with app.app_context():
+#     # db.session.add(first_task)    
+#     # db.session.add(second_task)
+#     # db.session.add(third_task)
+#     # db.session.commit()
+
+#     tasks = Task.query.all()
+#     for task in tasks:
+#         print(task.task)
